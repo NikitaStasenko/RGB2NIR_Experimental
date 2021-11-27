5449b780d532863f404d775203bdfb3131e65f0e
Maxim Savinov <cabuhob@gmail.com>
2021-11-27 17:55:15 +0300

    Updated script for downloading dataset

--- datasets/dowload_convert_apples_dataset.sh
+++ datasets/dowload_convert_apples_dataset.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Downloading dataset ..."
-# FIXME: change path to zip
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1yEiquJg9inIFgR3F-N5Z3DbFnXJ0aXmA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1yEiquJg9inIFgR3F-N5Z3DbFnXJ0aXmA" -O dataset/apples.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Un-_cwgPoSikJmW4hT7Y8T8S1f-FUVA3' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Un-_cwgPoSikJmW4hT7Y8T8S1f-FUVA3" -O datasets/apples_RGB_NIR.zip && rm -rf /tmp/cookies.txt
 
-# TODO: unzip
+echo "Unzip dataset ..."
+unzip -q datasets/apples_RGB_NIR.zip 
 
-rm dataset/apples.zip 
+echo "Remove zip file ..."
+rm datasets/apples_RGB_NIR.zip
